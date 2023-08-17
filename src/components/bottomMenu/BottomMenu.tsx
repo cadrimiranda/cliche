75d0711477e278199398383c98072fbc9ed34ea6
Carlos A Miranda <cadriano.miranda@gmail.com>
2023-08-17 07:53:22 -0300

    fix: icon buttom import

--- src/components/bottomMenu/BottomMenu.tsx
+++ src/components/bottomMenu/BottomMenu.tsx
@@ -1,4 +1,4 @@
-import { IconButton } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Unstable_Grid2";
 
 import HomeIcon from "@mui/icons-material/Home";
