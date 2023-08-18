f58b8630354b8242710c9893e14eeb31f22f643f
Carlos A Miranda <cadriano.miranda@gmail.com>
2023-08-18 14:57:11 -0300

    feat: added more fake book list

--- src/pages/home/components/BookList.tsx
+++ src/pages/home/components/BookList.tsx
@@ -1,15 +1,17 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { BookItem, BookListItem } from "./BookListItem";
+import { SxProps, Theme } from "@mui/material/styles";
 
 type BookListProps = {
   books: BookItem[];
   listTitle: string;
+  sx?: SxProps<Theme>;
 };
 
-export const BookList = ({ books, listTitle }: BookListProps) => {
+export const BookList = ({ books, listTitle, sx }: BookListProps) => {
   return (
-    <Box sx={{ mt: "20px", ml: "24px" }}>
+    <Box sx={{ mt: "20px", ml: "24px", ...sx }}>
       <Typography
         color={(theme) => theme.clicheTheme.colors.text.title}
         variant="h6"
@@ -23,6 +25,7 @@ export const BookList = ({ books, listTitle }: BookListProps) => {
           m: "10px 0 0",
           p: 0,
           overflowX: "auto",
+          overflowY: "hidden",
           "::-webkit-scrollbar": { display: "none" },
         }}
       >
