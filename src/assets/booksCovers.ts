6a0d3a73fdaf735ea3149e8d777305aa90087553
Carlos A Miranda <cadriano.miranda@gmail.com>
2023-08-18 07:10:28 -0300

    feat: added book covers assets

---
+++ src/assets/booksCovers.ts
@@ -0,0 +1,37 @@
+import Cover1 from "./bookCovers/cover1.png";
+import Cover2 from "./bookCovers/cover2.png";
+import Cover3 from "./bookCovers/cover3.png";
+import Cover4 from "./bookCovers/cover4.png";
+import Cover5 from "./bookCovers/cover5.png";
+import Cover6 from "./bookCovers/cover6.png";
+import Cover7 from "./bookCovers/cover7.png";
+import Cover8 from "./bookCovers/cover8.png";
+import Cover9 from "./bookCovers/cover9.png";
+import Cover10 from "./bookCovers/cover10.png";
+import Cover11 from "./bookCovers/cover11.png";
+import Cover12 from "./bookCovers/cover12.png";
+import Cover13 from "./bookCovers/cover13.png";
+import Cover14 from "./bookCovers/cover14.png";
+import Cover15 from "./bookCovers/cover15.png";
+import Cover16 from "./bookCovers/cover16.png";
+
+const allCovers = [
+  Cover1,
+  Cover2,
+  Cover3,
+  Cover4,
+  Cover5,
+  Cover6,
+  Cover7,
+  Cover8,
+  Cover9,
+  Cover10,
+  Cover11,
+  Cover12,
+  Cover13,
+  Cover14,
+  Cover15,
+  Cover16,
+];
+
+export { allCovers };
