9c88e6435e37a6db28c848e5b451b0116e1a488a
Carlos Adriano <cadriano.miranda@gmail.com>
2023-09-22 08:09:10 -0300

    cliche/#2: add animation to search

--- src/pages/search/component/SearchHeader.tsx
+++ src/pages/search/component/SearchHeader.tsx
@@ -1,10 +1,15 @@
-import Box from "@mui/material/Box";
+import Box, { BoxProps } from "@mui/material/Box";
 import { useClicheTheme } from "../../../app/theme/ClicheThemeProvider";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import textFieldClasses from "@mui/material/TextField/textFieldClasses";
 import SearchIcon from "@mui/icons-material/Search";
 import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import ArrowBack from "@mui/icons-material/ArrowBack";
+import { useState } from "react";
+import { inputBaseClasses, outlinedInputClasses } from "@mui/material";
 
 const EyeIcon = () => {
   const {
@@ -41,16 +46,93 @@ const EyeIcon = () => {
   );
 };
 
+const getStyles = (): { [key: string]: BoxProps['sx'] } => {
+  const mainStyle: BoxProps['sx'] = {
+    transition: "background-color 1s ease"
+  };
+
+  return {
+    background: {
+      ...mainStyle,
+      padding: "16px 32px 24px 32px",
+      backgroundColor: (t) => t.palette.warning.main,
+      position: "sticky",
+      top: 0,
+      borderBottomRightRadius: "50px",
+    },
+    backgroundSearch: {
+      ...mainStyle,
+      padding: '16px 32px',
+      borderBottom: '1px solid',
+      borderBottomColor: t => t.clicheTheme.colors.line,
+      [`.${textFieldClasses.root}`]: {
+        width: '85%',
+        [`.${inputBaseClasses.colorSecondary}`]: {
+          borderRadius: "8px",
+          backgroundColor: "#ECECEC",
+          [`.${outlinedInputClasses.notchedOutline}`]: {
+            borderRadius: "8px",
+          },
+        },
+      },
+    },
+    gridOut: {
+      WebkitAnimation:
+        "scale-out-ver-top 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both",
+      animation:
+        "scale-out-ver-top 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both",
+      "@-webkit-keyframes scale-out-ver-top": {
+        "0%": {
+          height: '150px',
+          WebkitTransform: "scaleY(1)",
+          transform: "scaleY(1)",
+          WebkitTransformOrigin: "100% 0%",
+          transformOrigin: "100% 0%",
+          opacity: 1
+        },
+        "100%": {
+          height: '0px',
+          WebkitTransform: "scaleY(0)",
+          transform: "scaleY(0)",
+          WebkitTransformOrigin: "100% 0%",
+          transformOrigin: "100% 0%",
+          opacity: 1
+        }
+      },
+      "@keyframes scale-out-ver-top": {
+        "0%": {
+          height: '150px',
+          WebkitTransform: "scaleY(1)",
+          transform: "scaleY(1)",
+          WebkitTransformOrigin: "100% 0%",
+          transformOrigin: "100% 0%",
+          opacity: 1
+        },
+        "100%": {
+          height: '0px',
+          WebkitTransform: "scaleY(0)",
+          transform: "scaleY(0)",
+          WebkitTransformOrigin: "100% 0%",
+          transformOrigin: "100% 0%",
+          opacity: 1
+        }
+      }
+    }
+  }
+}
+
 export const SearchHeader = () => {
+  const [isSearching, setIsSearching] = useState(false);
+
+  const handleSearchInteract = () => {
+    setIsSearching(true);
+  }
+
+  const { background, backgroundSearch, gridOut } = getStyles();
+
   return (
     <Box
-      sx={{
-        padding: "16px 32px 24px 32px",
-        backgroundColor: (t) => t.palette.warning.main,
-        position: "sticky",
-        top: 0,
-        borderBottomRightRadius: "50px",
-      }}
+      sx={isSearching ? backgroundSearch : background}
     >
       <Grid2
         container
@@ -58,7 +140,7 @@ export const SearchHeader = () => {
         justifyContent="space-between"
         alignItems="flex-end"
         flexWrap="nowrap"
-        sx={{ height: "150px" }}
+        sx={isSearching ? gridOut : {}}
       >
         <Typography
           color={(t) => t.clicheTheme.colors.buttons.active}
@@ -71,18 +153,29 @@ export const SearchHeader = () => {
         </Typography>
         <EyeIcon />
       </Grid2>
-      <TextField
-        placeholder="Pesquise aqui"
-        sx={{ mt: "14px" }}
-        color="secondary"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
+      <Grid2 container flexDirection="row" alignItems="center" justifyContent="space-between">
+        {
+          isSearching ? (
+            <IconButton onClick={() => setIsSearching(false)} color="secondary">
+              <ArrowBack />
+            </IconButton>
+          ) : null
+        }
+        <TextField
+          placeholder="Pesquise aqui"
+          sx={{ mt: isSearching ? '0' : "14px" }}
+          color="secondary"
+          onClick={handleSearchInteract}
+          onFocus={handleSearchInteract}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid2>
     </Box>
   );
 };
